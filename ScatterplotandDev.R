b084da537c835ead65dce5187242f7f82b3bf5af
Marceltheman1 <60233123+Marceltheman1@users.noreply.github.com>
2020-03-05 19:06:47 -0500

    Create ScatterplotandDev.R

---
+++ ScatterplotandDev.R
@@ -0,0 +1,5 @@
+projectData = read.table("Sea Ice and Temperature Data.txt", sep = ",", header = T) #reads our data into the global environment
+attach(projectData) 
+
+boxplot(Temperature.Anomalies.based.on.20th.century.averages...C., names = c("Temperature.Anomalies.based.on.20th.century.averages...C."))
+boxplot(Minimum.Arctic.Sea.Ice.Extent..Millions.of.squared.km., names = c("Minimum.Arctic.Sea.Ice.Extent..Millions.of.squared.km."))
