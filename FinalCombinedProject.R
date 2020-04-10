8fdd78a4a16578eba51ff1c5da6351ecba9515bb
John Li <lij19@wit.edu>
2020-04-09 20:00:42 -0400

    Update FinalCombinedProject.R

--- FinalCombinedProject.R
+++ FinalCombinedProject.R
@@ -53,48 +53,48 @@ R = cor(Temperature.Anomalies.based.on.20th.century.averages...C., Minimum.Arcti
 #Question 2D: R Squared (Coefficient of Determination)
 RSquared = R*R
 
-
 #Question 2E
 qqnorm(Minimum.Arctic.Sea.Ice.Extent..Millions.of.squared.km.)#creates a plot with theoretical quantiles vs your actual data points
 qqline(Minimum.Arctic.Sea.Ice.Extent..Millions.of.squared.km.)#overlays a perfect distribution curve
 
 
-#Questions 3a: Minimum Arctic Sea Ice
-n_X = nrow(projectData)
-x_bar_X = mean(Minimum.Arctic.Sea.Ice.Extent..Millions.of.squared.km.)
-std_dev_X = sd(Minimum.Arctic.Sea.Ice.Extent..Millions.of.squared.km.)
-population_std_dev_X = sqrt(((std_dev_Xstd_dev_X)(n_X-1))/n_X)
-z_critical_value_95 = qnorm(1-(.05/2))#Critical Value 95% alpha =0.05
+#Questions 3a: Minimum Arctic Sea Ice (Dependent Variable = Y)
+n_Y = nrow(projectData)
+bar_Y = mean(Minimum.Arctic.Sea.Ice.Extent..Millions.of.squared.km.)
+std_dev_Y = sd(Minimum.Arctic.Sea.Ice.Extent..Millions.of.squared.km.)
+population_std_dev_Y = sqrt(((std_dev_Y)*(std_dev_Y)*(n_Y-1))/n_Y)
+z_critical_value_Y = qnorm(1-(.05/2))#Critical Value 95% alpha =0.05
 
 #Margin of Error
-z_margin_of_error_95 = (z_critical_value_95*(population_std_dev_X/sqrt(n_X)))
+z_margin_of_error_Y = (z_critical_value_Y*(population_std_dev_Y/sqrt(n_Y)))
 
 #Confidence Interval
-right_bound_z_95 = x_bar_X + z_margin_of_error_95
-left_bound_z_95 = x_bar_X - z_margin_of_error_95
-cat("The 95% Confidence Interval for the true population mean Sea Ice is: (", left_bound_z_95,",", right_bound_z_95)
+right_bound_z_Y = bar_Y + z_margin_of_error_Y
+left_bound_z_Y = bar_Y - z_margin_of_error_Y
+cat("The 95% Confidence Interval for the true population mean Sea Ice is: (", left_bound_z_Y,",", right_bound_z_Y,")")
 
-#Questions 3b: Temperature Anomaly
-n_Q1 = nrow(myFinalData)#sets sample size
-x_bar_Q1 = mean(Temperature.Anomalies.based.on.20th.century.averages...C.)#Finds the mean
-std_dev_Q1 = sd(Temperature.Anomalies.based.on.20th.century.averages...C.)#Finds standard diviation
-population_std_dev_q1 = sqrt(((std_dev_Q1)(std_dev_Q1)(n_Q1-1))/n_Q1)#Finds the population SD [needed for z]
-z_critical_value_95 = qnorm(1-(.05/2)) #Finds critical value for 95% (alpha = 0.05) --> 1-(alpha/2)
+
+
+#Questions 3b: Temperature Anomaly (Independent Variable = X)
+n_X = nrow(projectData)#sets sample size
+bar_X = mean(Temperature.Anomalies.based.on.20th.century.averages...C.)#Finds the mean
+std_dev_X = sd(Temperature.Anomalies.based.on.20th.century.averages...C.)#Finds standard diviation
+population_std_dev_X = sqrt(((std_dev_X)*(std_dev_X)*(n_X-1))/n_X)#Finds the population SD [needed for z]
+z_critical_value_X = qnorm(1-(.05/2)) #Finds critical value for 95% (alpha = 0.05) --> 1-(alpha/2)
 
 #Margin of Error
-z_margin_of_error_95 = (z_critical_value_95*(population_std_dev_q1/sqrt(n_Q1)))#Finds the margin of error for 95% CI
+z_margin_of_error_X = (z_critical_value_X*(population_std_dev_X/sqrt(n_X)))#Finds the margin of error for 95% CI
 
 #Confidence Interval
-right_bound_z_95 = x_bar_Q1 + z_margin_of_error_95
-left_bound_z_95 = x_bar_Q1 - z_margin_of_error_95
-cat("We are 95% confident for the true population mean temperature anomilies of the artic is: (",left_bound_z_95,",",right_bound_z_95,")")
+right_bound_z_X = bar_X + z_margin_of_error_X
+left_bound_z_X = bar_X - z_margin_of_error_X
+cat("We are 95% confident for the true population mean temperature anomilies of the artic is: (",left_bound_z_X,",",right_bound_z_X,")")
 
 
 #EXTRA
 ##Graphing
 plot(Year, Temperature.Anomalies.based.on.20th.century.averages...C., main = "Minimum Sea Ice Extent per Year", xlab = "Year",
      ylab = "Temperature Anomaly based on 20th Century Average (C)", pch = 21, bg = c("blue"))
-boxplot(Temperature.Anomalies.based.on.20th.century.averages...C., names = c("Temperature.Anomalies.based.on.20th.century.averages...C."), col = c("red"))
 
 
 
